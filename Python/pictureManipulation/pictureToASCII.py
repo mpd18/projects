76b1293b0132276fabe6969409e9b1a587652b92
Michael Dean <mpd18@Michael-Surface.localdomain>
2017-10-20 20:08:27 -0400

    fixed the python picture to ASCII code

--- Python/pictureManipulation/pictureToASCII.py
+++ Python/pictureManipulation/pictureToASCII.py
@@ -1,10 +1,10 @@
 def getRow(int,stringList):
 	i=0
-	bounds=[36,72,108,144,180,216,252]
-	for bound in bounds:
+	bounds=[36,72,108,144,180,216,256]
+	for i in range(0,len(bounds)):
 		if(int < bounds[i]):
 			return i
-		i+=1
+			
 def convertToGrey(RGB):
 	return (RGB[0] + RGB[1] + RGB[2])/3
 
@@ -13,10 +13,12 @@ import random
 import sys
 if(len(sys.argv) > 1):
 	
+	outfile = open("outfile.jpg","w");
 	#Read image
 	im = Image.open(sys.argv[1] )
 	#resize image
 	im = im.resize((512,255),Image.ANTIALIAS)
+	im.save(outfile,"JPEG")
 
 	greyscale = [
 				" ",
@@ -29,7 +31,7 @@ if(len(sys.argv) > 1):
 				"#%$"
 				]
 	string = ''
-
+	textFile = open('picture.txt',"w")
 	for y in range (0,im.size[1]):
 		for x in range (0,im.size[0]):
 			# subtract from 255 otherwise colors are inverted
@@ -37,8 +39,9 @@ if(len(sys.argv) > 1):
 			row = getRow(pixel,greyscale)
 			string += greyscale[row][random.randint(0,len(greyscale[row]) -1)]
 		string +='\n'
-	textFile = open('picture.txt',"w")
-	textFile.write(string)
+		#write the string to the file line by line
+		textFile.write(string)
+		string = ''
 	textFile.close()
 else:
 	print("no picture to convert")
